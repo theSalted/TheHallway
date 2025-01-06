37f12ebfda580249ad71b76852e752799bd48c58
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-01-06 14:03:12 -0600

    External Batch 1

---
+++ Assets/Externals/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace InputAssets
+{
+    public class UICanvasControllerInput : MonoBehaviour
+    {
+
+        [Header("Output")]
+        public InputManager starterAssetsInputs;
+
+        public void VirtualMoveInput(Vector2 virtualMoveDirection)
+        {
+            starterAssetsInputs.MoveInput(virtualMoveDirection);
+        }
+
+        public void VirtualLookInput(Vector2 virtualLookDirection)
+        {
+            starterAssetsInputs.LookInput(virtualLookDirection);
+        }
+
+        public void VirtualJumpInput(bool virtualJumpState)
+        {
+            starterAssetsInputs.JumpInput(virtualJumpState);
+        }
+
+        public void VirtualSprintInput(bool virtualSprintState)
+        {
+            starterAssetsInputs.SprintInput(virtualSprintState);
+        }
+        
+    }
+
+}
